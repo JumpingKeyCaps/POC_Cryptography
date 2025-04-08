026cb6d3d65ca9c46a446fea62aead2f5d3165e0
Samuel Lebailly <sam.meusa@gmail.com>
2025-04-08 21:41:32 +0200

    feature(res/values): Add new colors resources.

--- app/src/main/java/com/lebaillyapp/poc_cryptography/ui/theme/Color.kt
+++ app/src/main/java/com/lebaillyapp/poc_cryptography/ui/theme/Color.kt
@@ -9,3 +9,23 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val VeryDark = Color(0xFF131313)
+val LessDark = Color(0xFF191919)
+val GrayContrast = Color(0xFF2a2c2b)
+val YellowContrast = Color(0xFFFEDA87)
+
+val chartreuse1 = Color(0xFFFE7f2d)
+val gunMetal1 = Color(0xFF233d4d)
+
+val chartreuse2 = Color(0xFFf7b538)
+val gunMetal2 = Color(0xFF780116)
+
+val chartreuse3 = Color(0xFFe07a5f)
+val gunMetal3 = Color(0xFFf4f1de)
+
+val chartreuse = Color(0xFFe1bb80)
+val gunMetal = Color(0xFF352208)
+
+val chartreuse5 = Color(0xFFe0ff4f)
+val gunMetal5 = Color(0xFF00272b)
