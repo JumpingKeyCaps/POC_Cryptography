fa3cb6fb1c058aef1300ea6a39900679591ef518
Samuel Lebailly <sam.meusa@gmail.com>
2025-04-23 19:34:11 +0200

    feature(model): Add a new File parameter to the SelectedFile data model.

--- app/src/main/java/com/lebaillyapp/poc_cryptography/model/SelectedFile.kt
+++ app/src/main/java/com/lebaillyapp/poc_cryptography/model/SelectedFile.kt
@@ -1,10 +1,12 @@
 package com.lebaillyapp.poc_cryptography.model
 
 import android.net.Uri
+import java.io.File
 
 data class SelectedFile(
     val name: String,
     val extension: String,
     val uri: Uri,
-    val size: Long // taille en octets
+    val size: Long, // taille en octets
+    val file: File? = null
 )
