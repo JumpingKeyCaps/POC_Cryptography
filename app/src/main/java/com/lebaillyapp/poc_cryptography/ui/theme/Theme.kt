9ae6efa0803f8cef145da4afd733ac7caf3170dc
Samuel Lebailly <sam.meusa@gmail.com>
2025-05-10 23:19:40 +0200

    feature(ui/theme): Fix some color and theme issues (day/night mode).

--- app/src/main/java/com/lebaillyapp/poc_cryptography/ui/theme/Theme.kt
+++ app/src/main/java/com/lebaillyapp/poc_cryptography/ui/theme/Theme.kt
@@ -20,7 +20,8 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 fun POC_CryptographyTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -47,7 +48,7 @@ fun POC_CryptographyTheme(
         }
 
         darkTheme -> DarkColorScheme
-        else -> DarkColorScheme
+        else -> LightColorScheme
     }
 
     MaterialTheme(
